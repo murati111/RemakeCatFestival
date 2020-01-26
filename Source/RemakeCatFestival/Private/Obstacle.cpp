c2c0014e673e7ca856bc8cb35cf91ca18fe242c6
murati111 <murati111taisyo@gmail.com>
2020-01-27 03:12:07 +0900

    【Add】障害物を追加

---
+++ Source/RemakeCatFestival/Private/Obstacle.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Obstacle.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+AObstacle::AObstacle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	TArray<FName> tagsTmp;
+	tagsTmp.Add(ActorTag);
+	Tags = tagsTmp;
+
+	//SceneComponentÇRootÇ…ê›íË
+	USceneComponent* NewRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	if (NewRootComponent != nullptr)
+	{
+		RootComponent = NewRootComponent;
+	}
+
+	Ramune = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ramune"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> RamuneMeshObj(TEXT("/Game/Cat/Objects/Ramune/SM_Ramune.SM_Ramune"));
+	if (RamuneMeshObj.Succeeded())
+	{
+		Ramune->SetStaticMesh(RamuneMeshObj.Object);
+		Ramune->SetupAttachment(RootComponent);
+		Ramune->SetRelativeScale3D(FVector(0.25f));
+		Ramune->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetupAttachment(Ramune);
+	BoxComp->SetRelativeLocation(FVector(0.0f, 0.0f, 505.0f));
+	BoxComp->SetRelativeScale3D(FVector(4.25f, 4.25f, 15.5f));
+}
+
+// Called when the game starts or when spawned
+void AObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AObstacle::ReceiveDamage_Implementation(int32& point)
+{
+	UE_LOG(LogTemp, Log, TEXT("MyIntValue"));
+	point = 0;
+	Destroy();
+}
+
---
+++ Source/RemakeCatFestival/Public/Obstacle.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Public/CatInterface.h"
+#include "Obstacle.generated.h"
+
+UCLASS()
+class REMAKECATFESTIVAL_API AObstacle : public AActor, public ICatInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AObstacle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Obstacle)
+		FName ActorTag = "Obstacle";
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Obstacle)
+		class UStaticMeshComponent* Ramune;
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Obstacle)
+		class UBoxComponent* BoxComp;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "CatInterface")
+		void ReceiveDamage(int32& point);
+	virtual void ReceiveDamage_Implementation(int32& point);
+
+};
