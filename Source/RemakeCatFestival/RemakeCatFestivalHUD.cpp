80af8f18e1eebd5cc1a9ae25ad118382a4e2c7d2
murati111 <murati111taisyo@gmail.com>
2020-02-10 02:09:48 +0900

    【Change】コンストラクターエラーを消すように

--- Source/RemakeCatFestival/RemakeCatFestivalHUD.cpp
+++ Source/RemakeCatFestival/RemakeCatFestivalHUD.cpp
@@ -10,8 +10,8 @@
 ARemakeCatFestivalHUD::ARemakeCatFestivalHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
-	CrosshairTex = CrosshairTexObj.Object;
+	//static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	//CrosshairTex = CrosshairTexObj.Object;
 }
 
 
